14364ef1a90124ded5bba35c621b52604e732a16
Mẫn <vanman.lqd@gmail.com>
2018-01-29 20:12:04 +0700

    Add exercise 1

---
+++ Exercise_1/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace Exercise_1
+{
+    [Activity(Label = "Exercise_1", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
