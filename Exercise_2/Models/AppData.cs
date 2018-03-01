c0d6c08a5ebbc93f7d126490ff89331d158da233
Mẫn <vanman.lqd@gmail.com>
2018-03-01 21:52:33 +0700

    handle logged status data

---
+++ Exercise_2/Models/AppData.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Exercise_2.Models
+{
+    public class AppData
+    {
+        [JsonProperty("is_logged_in")]
+        public bool IsLoggedIn { get; set; }
+    }
+}
