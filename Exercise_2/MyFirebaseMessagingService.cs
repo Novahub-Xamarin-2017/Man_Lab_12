0cc1551b7441829f451221b22028c7e7d8ab7567
Mẫn <vanman.lqd@gmail.com>
2018-03-15 20:09:26 +0700

    refactor code

--- Exercise_2/MyFirebaseMessagingService.cs
+++ Exercise_2/MyFirebaseMessagingService.cs
@@ -9,7 +9,7 @@ namespace Exercise_2
     [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
     public class MyFirebaseMessagingService : FirebaseMessagingService
     {
-        const string Tag = "MyFirebaseMsgService";
+        private const string Tag = "MyFirebaseMsgService";
         public override void OnMessageReceived(RemoteMessage message)
         {
             Log.Debug(Tag, "From: " + message.From);
@@ -19,6 +19,17 @@ namespace Exercise_2
             intent.PutExtra("message", message.GetNotification().Body);
             intent.PutExtra("from", message.GetNotification().Title);
             StartActivity(intent);
+
+            var builder = new Notification.Builder(this)
+                .SetContentTitle(message.From)
+                .SetContentText(message.GetNotification().Body)
+                .SetSmallIcon(Resource.Drawable.bug);
+            var notification = builder.Build();
+            notification.Defaults |= NotificationDefaults.Vibrate;
+            var notificationManager =
+                GetSystemService(NotificationService) as NotificationManager;
+            const int notificationId = 0;
+            notificationManager?.Notify(notificationId, notification);
         }
     }
 }
